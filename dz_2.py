542fcd88f9bd28b6dc178b0d5720a80768234dcf
alexey527 <nimblealexx@gmail.com>
2018-12-27 18:11:25 +0300

    изменил

--- dz_2.py
+++ dz_2.py
@@ -4,15 +4,15 @@ for i in range(n):
     x = int(input())
     mas.append(x)
 
-m = 0 # сумма
+summa = 0 # сумма
 b = 0
 a = 0
 maxim = 0
 for i in range(n):
     if mas[i] > 0 or mas[i] % 3 == 0:
-        m += mas[i]
+        summa += mas[i]
         b += 1
     if mas[i] > 0 or mas[i] % 3 == 0 and mas[i] > b:
         maxim = mas[i]
-sred_ar = m / b
-print(m, b, maxim, sred_ar)
+sred_ar = summa / b
+print(summa, b, maxim, sred_ar)
