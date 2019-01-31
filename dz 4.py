fa4ebe25f24d3d579248387097e87ea753816e74
alexey527 <nimblealexx@gmail.com>
2019-01-31 17:37:02 +0300

    добавлна программа

---
+++ dz 4.py
@@ -0,0 +1,29 @@
+def kolvo(a):
+    k = 0
+    while(a != 0):
+        a = a // 10
+        k += 1
+    return(k)
+
+
+
+
+
+
+
+
+
+
+n = int(input())
+mas = []
+for i in range(n):
+    x = int(input())
+    mas.append(x)
+
+itg = []
+g = 0
+for j in range(n):
+    s = kolvo(mas[j])
+    itg.append(s)
+print(itg)
+    
